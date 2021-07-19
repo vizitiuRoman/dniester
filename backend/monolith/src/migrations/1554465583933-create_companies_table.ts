9e0ed790baf348aaf5ad24ffd4b38324eea0fa55
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-19 21:23:46 +0300

    Update create company model query

--- backend/monolith/src/migrations/1554465583933-create_companies_table.ts
+++ backend/monolith/src/migrations/1554465583933-create_companies_table.ts
@@ -6,14 +6,15 @@ export class CreateCompaniesTable1554465583933 implements MigrationInterface {
             CREATE TABLE "companies"
             (
                 "id"            uuid              NOT NULL DEFAULT uuid_generate_v4(),
-                "created_at"    TIMESTAMP         NOT NULL DEFAULT now(),
-                "updated_at"    TIMESTAMP         NOT NULL DEFAULT now(),
                 "full_name"     character varying,
                 "email"         character varying,
                 "password"      character varying,
+                "created_at"    TIMESTAMP         NOT NULL DEFAULT now(),
+                "updated_at"    TIMESTAMP         NOT NULL DEFAULT now(),
                 CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"),
                 CONSTRAINT "PK_a1ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
-            )`);
+            );
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
