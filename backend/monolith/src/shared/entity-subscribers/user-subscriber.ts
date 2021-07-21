32836344f9f6dc4678155946eb5bc1bc67f6be1e
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-21 20:54:52 +0300

    Add event subscriber for user

---
+++ backend/monolith/src/shared/entity-subscribers/user-subscriber.ts
@@ -0,0 +1,31 @@
+import type {
+    EntitySubscriberInterface,
+    InsertEvent,
+    UpdateEvent,
+} from 'typeorm';
+import { EventSubscriber } from 'typeorm';
+
+import { UserEntity } from '../../modules/user/user.entity';
+import { UtilsService } from '../../providers/utils.service';
+
+@EventSubscriber()
+export class UserSubscriber
+    implements EntitySubscriberInterface<UserEntity> {
+    listenTo() {
+        return UserEntity;
+    }
+    beforeInsert(event: InsertEvent<UserEntity>) {
+        if (event.entity.password) {
+            event.entity.password = UtilsService.generateHash(
+                event.entity.password,
+            );
+        }
+    }
+    beforeUpdate(event: UpdateEvent<UserEntity>) {
+        if (event.entity.password !== event.databaseEntity.password) {
+            event.entity.password = UtilsService.generateHash(
+                event.entity.password,
+            );
+        }
+    }
+}
