2ac52aa63a0ecefd214466988983c006e3e9229d
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-22 13:50:05 +0300

    Change imports

--- backend/monolith/src/modules/company-auth/company-auth.module.ts
+++ backend/monolith/src/modules/company-auth/company-auth.module.ts
@@ -4,15 +4,18 @@ import { PassportModule } from '@nestjs/passport';
 import { CompanyModule } from '../company/company.module';
 import { CompanyAuthController } from './company-auth.controller';
 import { CompanyAuthService } from './company-auth.service';
-import { JwtStrategy } from './jwt.strategy';
+import { CompanyJwtStrategy } from './company-jwt.strategy';
 
 @Module({
     imports: [
         forwardRef(() => CompanyModule),
-        PassportModule.register({ defaultStrategy: 'jwt' }),
+        PassportModule.register({ defaultStrategy: 'company_jwt' }),
     ],
     controllers: [CompanyAuthController],
-    providers: [CompanyAuthService, JwtStrategy],
-    exports: [PassportModule.register({ defaultStrategy: 'jwt' }), CompanyAuthService],
+    providers: [CompanyAuthService, CompanyJwtStrategy],
+    exports: [
+        PassportModule.register({ defaultStrategy: 'company_jwt' }),
+        CompanyAuthService,
+    ],
 })
 export class CompanyAuthModule {}
