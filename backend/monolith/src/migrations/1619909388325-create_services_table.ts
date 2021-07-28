5edefee0e1e283ba255451822ddcf4dc5c5e1d65
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-28 13:36:38 +0300

    #time 2m #comment prettier

--- backend/monolith/src/migrations/1619909388325-create_services_table.ts
+++ backend/monolith/src/migrations/1619909388325-create_services_table.ts
@@ -5,12 +5,12 @@ export class createServicesTable1619909388325 implements MigrationInterface {
         await queryRunner.query(`
             CREATE TABLE "services"
             (
-                "id"            uuid              NOT NULL DEFAULT uuid_generate_v4(),
-                "name"          character varying,
-                "company_id"    uuid              NOT NULL,
-                "calendar_settings"      jsonb    NOT NULL default '{"start": "","end": "", "interval": null}'::jsonb,
-                "created_at"    TIMESTAMP         NOT NULL DEFAULT now(),
-                "updated_at"    TIMESTAMP         NOT NULL DEFAULT now(),
+                "id"                     uuid              NOT NULL DEFAULT uuid_generate_v4(),
+                "name"                   character varying,
+                "company_id"             uuid              NOT NULL,
+                "calendar_settings"      jsonb             NOT NULL default '{"start": "","end": "", "interval": null}'::jsonb,
+                "created_at"             TIMESTAMP         NOT NULL DEFAULT now(),
+                "updated_at"             TIMESTAMP         NOT NULL DEFAULT now(),
                 CONSTRAINT "PK_a3ffb1sf16421b9fc6f907b7433" PRIMARY KEY ("id")
             );
         `);
