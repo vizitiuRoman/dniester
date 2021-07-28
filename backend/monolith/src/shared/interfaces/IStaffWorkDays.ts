3f0184529e7030836eb5249d9683dba6a3f3dc73
vizitiuRoman <vizitiuroman.md@gmail.com>
2021-07-28 13:04:38 +0300

    #time 10m #comment staff work days interface

---
+++ backend/monolith/src/shared/interfaces/IStaffWorkDays.ts
@@ -0,0 +1,10 @@
+export interface IStaffWorkDays {
+    Day: string;
+    Index: number;
+    Enable: boolean;
+    WorkStartHour: Date;
+    WorkEndHour: Date;
+    BreakStartHour: Date;
+    BreakEndHour: Date;
+    State: string;
+}
